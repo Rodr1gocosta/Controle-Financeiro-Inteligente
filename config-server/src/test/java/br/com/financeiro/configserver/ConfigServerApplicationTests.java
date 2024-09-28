a8be1adae13189d93bbd431072640d1ba6c6e143
Rodrigo Willian Costa <89426247+Rodr1gocosta@users.noreply.github.com>
2024-09-28 11:47:29 -0300

    Ajustar os applications, remover comentarios e ajustar tela de login. (#13)
    * Ajuste no application.yml e ignorar integração com euraka-server
    
    * Remover comentários no applications
    
    * Remover comentários no applications do financeiro
    
    * Ajustar tela de login e verificação de Roles

--- config-server/src/test/java/br/com/financeiro/configserver/ConfigServerApplicationTests.java
+++ config-server/src/test/java/br/com/financeiro/configserver/ConfigServerApplicationTests.java
@@ -2,8 +2,10 @@ package br.com.financeiro.configserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ConfigServerApplicationTests {
 
 	@Test
