5a86ae53e3b5b6960370f2202168a2c514d5385d
Rodrigo Willian Costa <89426247+Rodr1gocosta@users.noreply.github.com>
2024-10-07 01:13:32 -0300

    Adicionar Teste de Integração (#18)
    * Ajuste de design na tela home
    
    * Adicionar verificador de permissao de usuario
    
    * Ajuste no tamanho do body
    
    * Ajuste na tela de login
    
    * Ajuste no application.yml e ignorar integração com euraka-server
    
    * Remover comentários no applications
    
    * Remover comentários no applications do financeiro
    
    * Ajustar tela de login e verificação de Roles
    
    * Update workflow.yml
    
    * Update workflow.yml
    
    * Adicionar dockerfile no gateway
    
    * Update workflow.yml
    
    * Criar dockerfile no microsservice seguranca e atualizar workflow.yml
    
    * Criar dockerfile no microsservice financeiro e atualizar workflow.yml
    
    * Criar dockerfile no microsservice notificacao e atualizar workflow.yml
    
    * Ajustar dockerfile - Notificacao
    
    * Remover instance - service-registry
    
    * Adicionar autenticacao - service-registry
    
    * Cadastrar autenticacao no service-registry - config-server
    
    * Adicionar spring security - config-server
    
    * Arquitetura dspring security- seguranca
    
    * Atualizar versões Java, Spring Boot e Spring Cloud - Seguranca
    
    * Atualizar versões Java, Spring Boot e Spring Cloud - service-registry
    
    * Atualizar versões Java, Spring Boot e Spring Cloud - config-server
    
    * Atualizar versões Java, Spring Boot e Spring Cloud - gateway
    
    * Adicionar spring security - financeiro
    
    * Atualizar versão Java, Spring Boot e Spring Cloud - financeiro
    
    * Atualizar versão Java, Spring Boot e Spring Cloud - notificacao
    
    * Atualizar versão Java no Dockerfile do projeto
    
    * Atualizar versão maven compiler pom.xml - geral
    
    * Atualizar versão java 21 - workflow
    
    * Nova versao v1.0.2-SNAPSHOT
    
    * Remover teste no build gateway - workflow
    
    * Remover arquivo de teste não utilizado - config-server
    
    * Adicionar teste de integração - seguranca
    
    * Adicionar teste de integracao - seguranca
    
    * Adicionar teste de integracao - financeiro, notificacao
    
    * Ajuste nos container service - Geral

--- config-server/src/test/java/br/com/financeiro/configserver/ConfigServerApplicationTests.java
+++ config-server/src/test/java/br/com/financeiro/configserver/ConfigServerApplicationTests.java
@@ -2,10 +2,8 @@ package br.com.financeiro.configserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class ConfigServerApplicationTests {
 
 	@Test
